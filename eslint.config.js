97bea72046095a4cd63b1415bbe25d8178ef5843
alissonengineer <alisson.engineerrr@gmail.com>
2026-09-15 23:10:57 +0600

    chore: add sass design tokens

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,10 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(js.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(
+  {
+    ignores: ['dist'],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+);
